88a40b0e23839c3e236af4a49698d52b19803203
Stephen Sizer <steve.sizer@gmail.com>
2019-06-09 19:35:30 +0100

    Init

---
+++ js/scripts.js
@@ -0,0 +1,82 @@
+var quotes = [
+  {
+    quote: "I have always believed that each man makes his own happiness and is responsible for his own problems. It is a simple philosophy.",
+    source: "Ray Kroc"
+  },
+  {
+    quote: "When we have respect for ourselves and others, we gravitate towards connections that encourage that.",
+    source: "Simeon Lindstrom, Codependency"
+  },
+  {
+    quote: "Anger is the ultimate destroyer of your own peace of mind.",
+    source: "Dalai Lama"
+  },
+  {
+    quote: "A man should have the aim and the determination to be honest and upright and sincere in all that he undertakes. If he adds persistency to this he can hardly help being successful",
+    source: "L. R. Ellert"
+  },
+  {
+    quote: "Only one thing is ever guaranteed, that is that you will definitely not achieve the goal if you don't take the shot.",
+    source: "Wayne Gretzky"
+  },
+  {
+    quote: "Don't be afraid. Be focused. Be determined. Be hopeful. Be empowered.",
+    source: "Michelle Obama "
+  },
+  {
+    quote: "The fact is that grief today is a family matter as much a s it is an individual one.",
+    source: "Barbara Okun"
+  },
+  {
+    quote: "Children really brighten up a household. They never turn the lights off.",
+    source: "Ralph Bus"
+  },
+  {
+    quote: "No one would have crossed the ocean if he could have gotten off the ship in the storm.",
+    source: "Charles Kettering"
+  },
+  {
+    quote: "Congratulations! today is your day. You're off to Great Places! You're off and away.",
+    source: "Dr. Seuss"
+  },
+  {
+    quote: "Appreciate those early influences and what they've done for you.",
+    source: "Willie Davis"
+  },
+  {
+    quote: "Emotional empathy is what motivates us to help others.",
+    source: "Brian Goldman"
+  },
+  {
+    quote: "The true wealth of a nation lies not in it's gold or silver but in it's learning, wisdom and in the uprightness of its sons.",
+    source: "Kahlil Gibran"
+  },
+  {
+    quote: "Make the decision, make another. Remake one past, you cannot.",
+    source: "Yoda"
+  },
+  {
+    quote: "Be honest in your feelings, for they are the surest conduit to knowledge...",
+    source: "Anoon Bondara"
+  },
+  {
+    quote: "I think beauty comes from actually knowing who you are. That's real beauty to me.",
+    source: "Elllen DeGeneres"
+  },
+  {
+    quote: "Memories, pressed between the pages of my mind. Memories, sweetened through the ages just like wine.",
+    source: "Elvis Presley"
+  },
+  {
+    quote: "If you want to see a rainbow you have to learn to see the rain.",
+    source: "Paulo Coelho"
+  },
+  {
+    quote: "I had it in my heart. I believed in myself, and I had confidence. I knew how to do it, had natural talent and I pursued it.",
+    source: "Muhammad Ali "
+  },
+  {
+    quote: "If I can help people I'll do it by giving them a chance to help themselves; and if I can uplift or inspire, let it be by example, inference and suggestion, rather than by injunction and dictation.",
+    source: "Elbert Hubbard"
+  },
+];
